c5709adf491348fae524c7f135e2e263c399e23e
Lucca Bringhenti <bringhentilucca@gmail.com>
2020-09-24 08:15:15 -0300

    Initial Commit - AT02

---
+++ AT02/Models/Pacote.cs
@@ -0,0 +1,13 @@
+namespace AT02.Models
+{
+    public class Pacote
+    {
+        public string Nome {get; set;}
+        public string Origem {get; set;}
+        public string Destino {get; set;}
+        public string Atrativos {get; set;}
+        public string Saida {get; set;}
+        public string Retorno {get; set;}
+        public int IdCriador {get; set;}
+    }
+}
---
+++ AT02/Models/Usuario.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace AT02.Models
+{
+    public class Usuario
+    {
+        public string Nome {get; set;}
+        public DateTime Nascimento {get; set;}
+        public string Login {get; set;}
+        public int Tipo {get; set;}
+    }
+}
